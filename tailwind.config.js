a63dc4b1eab4d1e81d1422d4bcbbfbe940e321d1
Andre Clark <andre@cosmicdivision.dev>
2020-02-14 11:23:06 -0800

    :sparkles: implement accent color
    Co-Authored-By: Nathan Goullette <nathangoullette@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,7 @@ module.exports = {
             colors: {
                 primary: "#b2b6c6",
                 secondary: "#181718",
-                deeppurple: {
-                    '900': darken(.25, "#44337a"),
-                    '800': darken(.225, "#44337a"),
-                    '700': darken(.2, "#44337a"),
-                    '600': darken(.175, "#44337a"),
-                    '500': darken(.15, "#44337a"),
-                    '400': darken(.125, "#44337a"),
-                    '300': darken(.1, "#44337a"),
-                    '200': darken(.05, "#44337a"),
-                    '100': darken(.025, "#44337a"),
-                }
+                accent: "#a88439",
             }
         }
     },
